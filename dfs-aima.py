e945ef48d33bfa33ffc0e63e30ad9e8d451d2abc
Arlann <handikaarlan@gmail.com>
2024-10-06 15:00:12 +0700

    fix: dict for graph

--- dfs-aima.py
+++ dfs-aima.py
@@ -2,16 +2,16 @@ from aima.search import GraphProblem, UndirectedGraph, depth_first_graph_search
 
 romania_map = UndirectedGraph(dict(
     Manchester = dict(Liverpool=None, Sheffield=None),
-    Liverpool = dict(Manchester=None, Shrewsbury=None),
+    Liverpool = dict(Manchester=None, Shrewsbury=None, Nottingham=None),
     Sheffield = dict(Manchester=None, Nottingham=None),
-    Shrewsbury = dict(Liverpool=None, Aberystwyth=None, Birmingham=None),
-    Nottingham = dict(Sheffield=None, Oxford=None, Birmingham=None),
+    Shrewsbury = dict(Liverpool=None, Aberystwyth=None, Birmingham=None, Cardiff=None),
+    Nottingham = dict(Sheffield=None, Oxford=None, Birmingham=None, Liverpool=None),
     Aberystwyth = dict(Shrewsbury=None, Cardiff=None),
-    Birmingham = dict(Shrewsbury=None, Nottingham=None, Bristol=None),
-    Cardiff = dict(Aberystwyth=None, Bristol=None),
+    Birmingham = dict(Shrewsbury=None, Nottingham=None, Bristol=None, Oxford=None),
+    Cardiff = dict(Aberystwyth=None, Bristol=None, Shrewsbury=None),
     Bristol = dict(Birmingham=None, Cardiff=None, Southampton=None),
-    Oxford = dict(Nottingham=None),
-    Southampton = dict(Bristol=None),
+    Oxford = dict(Nottingham=None, Birmingham=None, Southampton=None),
+    Southampton = dict(Bristol=None, Oxford=None),
 ))
 
 start_city = input("Input your starting city: ").strip().title()
